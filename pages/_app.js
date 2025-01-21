a4319c2602c54aa594928c436ab9aede8a7a496b
KrrishBhandari <bhandarikrishna057@gmail.com>
2025-01-21 18:58:42 +0530

    unwanted files removed

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,6 @@
 import "@/styles/globals.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import "@/styles/pivotchart.css";
-import "@/styles/BlockNote.css";
 import { ThemeProvider } from "@/context/ThemeContext";
 
 export default function App({ Component, pageProps }) {
